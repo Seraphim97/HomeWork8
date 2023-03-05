f65cc8e1c204b13036d26769f9c6eee18b2b8cca
Seraphim97 <savichevserafim@gmail.com>
2023-03-05 15:23:15 +0200

    class name changed, constant ACCURACY added, empty lines between tests added

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -17,21 +17,18 @@ public class CalcTest {
     }
 
     @Test
-
     public void divisionPositiveNumbers() {
         double result = calculator.division(8.8,2.5);
         Assert.assertEquals(3.52,result,0.001);
     }
 
     @Test
-
     public void divisionNegativeNumbers(){
         double result = calculator.division(-9.9, -3.3);
         Assert.assertEquals(3.0, result, 0.001);
     }
 
     @Test
-
     public void divisionZeroDividend() {
         double result = calculator.division(0, 5.5);
         Assert.assertEquals(0,result,0.001);
@@ -39,28 +36,24 @@ public class CalcTest {
     }
 
     @Test
-
     public void divisionZeroDivisor() {
         double result = calculator.division(5.5,0);
         Assert.assertEquals( Infinity, result, 0.001);
     }
 
     @Test
-
     public void multiplicationPositiveNumbers() {
         double result = calculator.multiplication(4.5, 3.4);
         Assert.assertEquals(15.299, result, 0.001);
     }
 
     @Test
-
     public void multiplicationNeganiveNumbers() {
         double result = calculator.multiplication(-7.7,-3.3);
         Assert.assertEquals(25.41,result,0.001);
     }
 
     @Test
-
     public void multiplicationZero() {
         double result = calculator.multiplication(0, 6.6);
         Assert.assertEquals(0, result, 0.001);
--- src/test/java/org/example/SolverTest.java
+++ src/test/java/org/example/SolverTest.java
@@ -4,22 +4,26 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class Solvertest {
+public class SolverTest {
     Solver solver;
+
     @Before
     public void setUpEachTest() {
         solver = new Solver();
     }
+
     @Test
     public void checkDiscriminantPositive() {
         int result = solver.squareNumbers(1,60,2);
         Assert.assertEquals(2,result);
     }
+
     @Test
     public void checkDiscriminantNegative() {
         int result = solver.squareNumbers(3,5,7);
         Assert.assertEquals(0, result);
     }
+
     @Test
     public void checkDiscriminantZero() {
         int result = solver.squareNumbers(11,22,11);
